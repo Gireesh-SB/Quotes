73316b186fae9f3585615da2fb15816339f7b82f
Gireesh SB <gireeshsb@live.com>
2021-08-02 00:19:28 +0530

    image optimized

--- src/app/quote.service.ts
+++ src/app/quote.service.ts
@@ -13,32 +13,32 @@ export class QuoteService {
           "author": "Mahatma Gandhi",
           "message": "An eye for an eye only ends up making the whole world blind.",
           "profile": "https://en.wikipedia.org/wiki/Mahatma_Gandhi",
-          "image": "https://upload.wikimedia.org/wikipedia/commons/d/d1/Portrait_Gandhi.jpg"
+          "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Mahatma-Gandhi%2C_studio%2C_1931.jpg/192px-Mahatma-Gandhi%2C_studio%2C_1931.jpg"
       },
       {
           "author": "Dr. A.P.J Abdul Kalam",
           "message": "All birds find shelter during the rain. But eagle avoids rain by flying over the clouds.",
-          "image": "https://upload.wikimedia.org/wikipedia/commons/b/b0/A._P._J._Abdul_Kalam_in_2008.jpg"
+          "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b0/A._P._J._Abdul_Kalam_in_2008.jpg/180px-A._P._J._Abdul_Kalam_in_2008.jpg"
       },
       {
           "author": "Steve Jobs",
           "message": "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking.",
-          "image": "https://upload.wikimedia.org/wikipedia/commons/f/f5/Steve_Jobs_Headshot_2010-CROP2.jpg"
+          "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dc/Steve_Jobs_Headshot_2010-CROP_%28cropped_2%29.jpg/248px-Steve_Jobs_Headshot_2010-CROP_%28cropped_2%29.jpg"
       },
       {
           "author": "Anne Frank",
           "message": "Whoever is happy will make others happy too.",
-          "image": "https://upload.wikimedia.org/wikipedia/commons/4/41/AnneFrankSchoolPhoto_%28cropped%29.jpg"
+          "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/AnneFrank1940_crop.jpg/209px-AnneFrank1940_crop.jpg"
       },
       {
           "author": "Eleanor Roosevelt",
           "message": "The future belongs to those who believe in the beauty of their dreams.",
-          "image": "https://upload.wikimedia.org/wikipedia/commons/2/22/Eleanor_Roosevelt_portrait_1933.jpg"
+          "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Eleanor_Roosevelt_portrait_1933.jpg/198px-Eleanor_Roosevelt_portrait_1933.jpg"
       },
       {
           "author": "Nelson Mandela",
           "message": "The greatest glory in living lies not in never falling, but in rising every time we fall.",
-          "image": "https://upload.wikimedia.org/wikipedia/commons/0/02/Nelson_Mandela_1994.jpg"
+          "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/Nelson_Mandela_1994.jpg/185px-Nelson_Mandela_1994.jpg"
       }      
     ];
   }
